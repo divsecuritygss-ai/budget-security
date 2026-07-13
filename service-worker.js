35c2547cf283199d832650d00c2c94b01d08a264
divsecuritygss-ai <div.securitygss@gmail.com>
2026-07-13 22:00:30 +0700

    Fix casing of index.html to Index.html

--- service-worker.js
+++ service-worker.js
@@ -3,7 +3,7 @@ const OFFLINE_URL = '/offline.html';
 
 const ASSETS_TO_CACHE = [
   '/',
-  '/index.html',
+  '/Index.html',
   OFFLINE_URL,
   'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css',
   'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css',
